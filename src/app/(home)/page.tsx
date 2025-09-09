eb5e6a95c021f584d854510df22d7f8a4d366493
HyeonjeongJang <hyeonjeong.jang.0302@gmail.com>
2025-09-09 16:21:00 +0900

    tRPC 세팅

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,19 @@
-export default function Home() {
-  return(
-    <div>
-      I will load videos in the future!
-    </div>
-  ) 
+import { HydrateClient, trpc } from "@/trpc/server";
+import { PageClient } from "./client";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+
+export default async function Home() {
+  void trpc.hello.prefetch({ text: "Hana" });
+  // src/app/(home)/client.tsx의 useSuspenseQuery와 쌍을 이룸
+
+  return (
+    <HydrateClient>
+      <Suspense fallback={<div>Loading...</div>}>
+        <ErrorBoundary fallback={<div>Error...</div>}>
+          <PageClient />
+        </ErrorBoundary>
+      </Suspense>
+    </HydrateClient>
+  )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
-import {Inter} from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
-import {ClerkProvider} from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
+import { TRPCProvider } from "@/trpc/client";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,9 +23,11 @@ export default function RootLayout({
         <body
           className={inter.className}
         >
-          {children}
+          <TRPCProvider>
+            {children}
+          </TRPCProvider>
         </body>
       </html>
-    </ClerkProvider>    
+    </ClerkProvider>
   );
 }
