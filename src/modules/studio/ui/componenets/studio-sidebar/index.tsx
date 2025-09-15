4cbe85d998b4239630f27b14e50c41045e9d218c
HyeonjeongJang <hyeonjeong.jang.0302@gmail.com>
2025-09-15 17:06:56 +0900

    Studio 사이드바 아바타 수정 중

--- src/modules/studio/ui/componenets/studio-sidebar/index.tsx
+++ src/modules/studio/ui/componenets/studio-sidebar/index.tsx
@@ -1,17 +1,39 @@
-import { Sidebar, SidebarContent, SidebarGroup, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "@/components/ui/sidebar";
-import { MainSection } from "./main-section";
-import { Separator } from "@/components/ui/separator";
+"use client";
+
 import Link from "next/link";
-import { LogOutIcon } from "lucide-react";
+import { LogOutIcon, VideoIcon } from "lucide-react";
+import { usePathname } from "next/navigation";
+import { Separator } from "@/components/ui/separator";
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarGroup,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem
+} from "@/components/ui/sidebar";
+import { StudioSidebarHeader } from "./studio-sidebar-header";
+
+
 
 export const StudioSidebar = () => {
+    const pathname = usePathname();
+
     return (
         <Sidebar className="pt-16 z-40" collapsible="icon">
             <SidebarContent className="bg-background">
-                <MainSection />
-                <Separator />
                 <SidebarGroup>
+                    <StudioSidebarHeader/>
                     <SidebarMenu>
+                        <SidebarMenuItem>
+                            <SidebarMenuButton isActive={pathname === "/studio"} tooltip="Exit studio" asChild>
+                                <Link href="/studio">
+                                    <VideoIcon className="size-5" />
+                                    <span className="text-sm">Content</span>
+                                </Link>
+                            </SidebarMenuButton>
+                        </SidebarMenuItem>
+                        <Separator />
                         <SidebarMenuItem>
                             <SidebarMenuButton tooltip="Exit studio" asChild>
                                 <Link href="/">
