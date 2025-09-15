5c4ead9c8bf56180eb07d58daef4afe402591f9d
HyeonjeongJang <hyeonjeong.jang.0302@gmail.com>
2025-09-15 13:38:18 +0900

    사이드바 수정 중

--- src/modules/studio/ui/componenets/studio-sidebar/index.tsx
+++ src/modules/studio/ui/componenets/studio-sidebar/index.tsx
@@ -1,15 +1,27 @@
-import { Sidebar, SidebarContent } from "@/components/ui/sidebar";
+import { Sidebar, SidebarContent, SidebarGroup, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "@/components/ui/sidebar";
 import { MainSection } from "./main-section";
 import { Separator } from "@/components/ui/separator";
-import { PersonalSection } from "./personal-section";
+import Link from "next/link";
+import { LogOutIcon } from "lucide-react";
 
-export const StudioSidebar =()=>{
+export const StudioSidebar = () => {
     return (
-        <Sidebar className="pt-16 z-40 border-none" collapsible="icon">
+        <Sidebar className="pt-16 z-40" collapsible="icon">
             <SidebarContent className="bg-background">
-                <MainSection/>
-                <Separator/>
-                <PersonalSection/>
+                <MainSection />
+                <Separator />
+                <SidebarGroup>
+                    <SidebarMenu>
+                        <SidebarMenuItem>
+                            <SidebarMenuButton tooltip="Exit studio" asChild>
+                                <Link href="/">
+                                    <LogOutIcon className="size-5" />
+                                    <span className="text-sm">Exit studio</span>
+                                </Link>
+                            </SidebarMenuButton>
+                        </SidebarMenuItem>
+                    </SidebarMenu>
+                </SidebarGroup>
             </SidebarContent>
         </Sidebar>
     );
