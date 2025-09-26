5dc74b67fd6d1818dc635dad60d968cd11f382e6
hyeonjeongjang <hyeonjeong.jang.0302@gmail.com>
2025-09-26 17:30:03 +0900

    drizzle-orm: videoRelation 추가

--- src/modules/studio/ui/componenets/studio-upload-modal.tsx
+++ src/modules/studio/ui/componenets/studio-upload-modal.tsx
@@ -5,6 +5,7 @@ import { Button } from "@/components/ui/button";
 import { trpc } from "@/trpc/client";
 import { toast } from "sonner";
 import { ResponsiveModal } from "@/components/responsive-modal";
+import { StudioUploader } from "./studio-uploader";
 
 export const StudioUploadModal = () => {
   const utils = trpc.useUtils();
@@ -23,10 +24,14 @@ export const StudioUploadModal = () => {
       {/* !!: 값을 명시적으로 불리언(Boolean) 타입으로 변환하기 위해 !!를 사용 */}
       <ResponsiveModal
         title="Upload a video"
-        open={!!create.data}
+        open={!!create.data?.url}
         onOpenChange={() => create.reset()}
       >
-        <p>This will be an uploader</p>
+        {create.data?.url ? (
+          <StudioUploader endpoint={create.data.url} onSuccess={() => {}} />
+        ) : (
+          <Loader2Icon />
+        )}
       </ResponsiveModal>
       <Button
         variant="secondary"
